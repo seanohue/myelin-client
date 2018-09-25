23a850aea966003709020c4c1407a553b6743217
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-09-25 12:40:50 -0400

    Test msg output

--- tests/unit/Terminal.spec.js
+++ tests/unit/Terminal.spec.js
@@ -22,6 +22,8 @@ describe('Terminal.vue', () => {
   beforeEach(() => {
     sandbox = sinon.createSandbox()
     sandbox.stub($bus)
+    sandbox.stub($socket)
+    sandbox.stub($ansi)
   })
 
   it('sets up listeners on bus', () => {
@@ -33,5 +35,37 @@ describe('Terminal.vue', () => {
       'message'
     ]
     events.forEach(event => expect($bus.$on.calledWith(event)).to.be.true)
+  })
+
+  describe('outputting messages', () => {
+    beforeEach(() => {
+      $ansi.ansi_to_html.returnsArg(0)
+    })
+    it('should add a message to the messages list when method is called', () => {
+      const wrapper = shallowMount(Terminal, {mocks: { $bus, $socket, $ansi }})
+
+      const message = 'This is a test.'
+      wrapper.vm.outputMessage(message)
+
+      expect(wrapper.vm.messages).to.include(message)
+    })
+
+    it('should add a message to the messages list when method is called', () => {
+      const wrapper = shallowMount(Terminal, {mocks: { $bus, $socket, $ansi }})
+
+      const message = 'This is a test.'
+      wrapper.vm.outputMessage(message)
+
+      expect(wrapper.vm.messages).to.include(message)
+    })
+
+    it('should call to scroll', () => {
+      const wrapper = shallowMount(Terminal, {mocks: { $bus, $socket, $ansi }})
+      sandbox.spy(wrapper.vm, 'scrollDown')
+      const message = 'This is a test.'
+      wrapper.vm.outputMessage(message)
+
+      expect(wrapper.vm.scrollDown.called).to.equal(true)
+    })
   })
 })
