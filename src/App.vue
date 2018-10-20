36a305d4eb2f84c344e8498a4738a712452ead91
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-10-20 12:39:30 -0400

    More CSS finagling.

--- src/App.vue
+++ src/App.vue
@@ -39,10 +39,29 @@ body {
   left: 0;
   bottom: 0;
   right: 0;
-  background: linear-gradient(rgba(70, 74, 69, 0) 50%, rgba(9, 0, 87, 0.25) 50%);
+  background: linear-gradient(rgba(70, 74, 69, 0) 80%, rgba(9, 0, 87, 0.25) 20%);
   z-index: 2;
   background-size: 100% 2px, 3px 100%;
   pointer-events: none;
+}
+
+@keyframes flicker {
+  0% {
+  opacity: 0.27861;
+  }
+  20% {
+  opacity: 0.18128;
+  }
+  40% {
+  opacity: 0.26559;
+  }
+  60% {
+  opacity: 0.20313;
+  }
+  80% {
+  opacity: 0.51428;
+  }
+
 }
 
 .crt::after {
@@ -57,5 +76,6 @@ body {
   opacity: 0;
   z-index: 2;
   pointer-events: none;
+  animation: flicker 0.75s infinite;
 }
 </style>
