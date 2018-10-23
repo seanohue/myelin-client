bb780b419cb884e48a229957e1dd4048e6e92dc0
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-10-23 13:10:25 -0400

    Refactor audio to be quite generic now.

--- src/plugins/audio.js
+++ src/plugins/audio.js
@@ -1,6 +1,8 @@
 import partialRight from 'lodash/partialRight'
 import map from 'lodash/map'
 import find from 'lodash/find'
+import bind from 'lodash/bind'
+import capitalize from 'lodash/capitalize'
 
 import tracks from '@/data/tracks'
 
@@ -13,17 +15,20 @@ const AudioPlugin = {
 class MyelinAudio {
   init () {
     return import('howler')
-      .then(({ Howl, Howler }) => {
-        this.Howl = Howl
-        this.Howler = Howler
-        this.setupTracks()
-        return this.setupPlayers()
-          .then(() => {
-            this.ambient.play()
-            this.music.play()
-            return this
-          })
+      .then(bind(this.initHowler, this))
+      .then(bind(this.setupTracks, this))
+      .then(bind(this.setupPlayers, this))
+      .then(() => {
+        this.ambient.play()
+        this.music.play()
+        return this
       })
+  }
+
+  initHowler ({Howl, Howler}) {
+    this.Howl = Howl
+    this.Howler = Howler
+    return this
   }
 
   setupTracks () {
@@ -37,57 +42,87 @@ class MyelinAudio {
     return track
   }
 
-  findTrack (trackname) {
-    return find(this.tracks, {name: trackname})
-  }
-
   setupPlayers () {
-    return this.initAmbient()
-      .then(this.initMusic.bind(this))
+    return Promise.all([
+      this.initAmbient(),
+      this.initMusic()
+    ])
   }
 
   initAmbient () {
     const initial = this.findTrack('tubestatic')
-    const fetchInitial = initial.fetch
-    if (!fetchInitial) return Promise.resolve()
-    return fetchInitial()
-      .then((src) => {
-        initial.data = src.default
-        this.ambient = new this.Howl({
-          src: [src.default],
-          loop: true,
-          preload: true,
-          volume: 0.25,
-          rate: initial.rate,
-          onplayerror (id, err) {
-            console.log(`[Ambient Audio] PlayError: ${id} -- `, err)
-          }
-        })
+    return this.createPlayer('ambient', initial)
+      .then(howl => {
+        this.ambient = howl
         return this
       })
   }
 
   initMusic () {
     const initial = this.findTrack('VESSELACCESS')
-    const fetchInitial = initial.fetch
-    if (!fetchInitial) return Promise.resolve()
-    return fetchInitial()
-      .then((src) => {
-        this.music = new this.Howl({
-          src: [src.default],
-          loop: true,
-          preload: true,
-          volume: 0.75,
-          rate: initial.rate,
-          onloaderror (id, err) {
-            console.log(`[Music Audio] LoadError: ${id} -- `, err)
-          },
-          onplayerror (id, err) {
-            console.log(`[Music Audio] PlayError: ${id} -- `, err)
-          }
-        })
+    return this.createPlayer('music', initial)
+      .then(howl => {
+        this.music = howl
         return this
       })
+  }
+
+  findTrack (trackname) {
+    return find(this.tracks, {name: trackname})
+  }
+
+  createPlayer (player, track) {
+    const initTrack = track.src
+      ? bind(Promise.resolve, Promise, track.src)
+      : () => track.fetch().then(imported => imported.default)
+
+    return initTrack()
+      .then(src => {
+        return new this.Howl({
+          src: [src],
+          loop: player !== 'sfx',
+          preload: true,
+          rate: track.rate || 1,
+          volume: this.getVolume(player),
+          onloaderror (id, err) {
+            return this.logError(player, `LoadError: ${id} -- ${err}`)
+          },
+          onplayerror (id, err) {
+            return this.logError(player, `PlayError: ${id} -- ${err}`)
+          }
+        })
+      })
+  }
+
+  playTrack (player, trackname) {
+    const track = this.findTrack(trackname)
+    if (!track) {
+      return this.logError(player, `TrackNotFoundError: ${trackname}}`)
+    }
+    if (!this[player]) {
+      return this.logError(player, 'PlayerNotFoundError: uninitialized')
+    }
+
+    this[player].stop()
+
+    return this.createPlayer(player, track)
+      .then((howl) => {
+        this[player] = howl
+        this[player].play()
+        return this
+      })
+  }
+
+  getVolume (player) {
+    return {
+      music: 0.75,
+      ambient: 0.5,
+      sfx: 1
+    }[player] || 1
+  }
+
+  logError (type, message) {
+    return console.warn(`[${capitalize(type)} Audio] ${message}`)
   }
 }
 
