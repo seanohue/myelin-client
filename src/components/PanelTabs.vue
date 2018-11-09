b83280a4d95841128930961c2848f7c72a44559b
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2018-11-09 14:41:21 -0500

    Style tweaks and fixes for tabs.

--- src/components/PanelTabs.vue
+++ src/components/PanelTabs.vue
@@ -5,9 +5,7 @@
       v-for="(tab, index) in tabs"
       :key="index"
       :class="['tab', { active: tab.component === activePanel }]"
-      @click="switchTo(tab)">
-      {{tab.name.toUpperCase()}}
-    </button>
+      @click="switchTo(tab)">{{tab.name.toUpperCase()}}</button>
   </div>
   <slot></slot>
 </div>
@@ -36,6 +34,8 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@import '../app.less';
+
 .tab-container {
   height: 26px;
   display: flex;
@@ -48,7 +48,22 @@ export default {
   display: flex;
   align-content: center;
   font-family: 'Roboto Mono', monospace;
-  font-size: 2em;
-  line-height: 3em;
+  font-size: 16px;
+  line-height: 20px;
+  background-color: @green-middle;
+  border: 1px @term-background;
+  color: @term-foreground;
+}
+
+.active {
+  border: 2px @green-background;
+  background-color: @term-foreground;
+  color: @term-background;
+}
+
+.active:focus {
+  box-shadow: 0 0 5px rgb(81, 238, 131);
+  border-color: @green-foreground;
+  outline: 0 none;
 }
 </style>
